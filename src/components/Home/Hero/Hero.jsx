e878f6870180144f605bcfe8f874ea06d2303c2f
sabrina <sabrinaahmednitu@gmail.com>
2023-09-15 13:12:02 +0600

    hero section

--- src/components/Home/Hero/Hero.jsx
+++ src/components/Home/Hero/Hero.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import './Hero.css'
+import HeroImg from '../../../images/family.jpg'
 
 const Hero = () => {
     return (
-        <div>
-          
+      <div className="max-w-4xl mx-auto mt-12 mb-12">
+        <div className="grid gap-12 lg:grid-cols-2 sm:grid-cols-1">
+          <div className="hero-left text-black">
+            <p>Welcome to</p>
+            <h3 className="text-4xl mt-2 mb-2">Electronic Store</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus
+              optio, dolor dicta possimus a quibusdam nobis illo accusamus,
+              sapiente doloribus ea ducimus illum. Nisi pariatur quasi deleniti
+              id, cum maiores aliquid repudiandae, enim illo cupiditate beatae,
+              ipsa nam accusamus! Ut incidunt repudiandae animi officia ullam.
+              Molestiae odit asperiores dolores expedita.
+            </p>
+            <button className="btn text-white bg-indigo-400  border-0 mt-4">
+              Shop Now
+            </button>
+          </div>
+    
+            <div className="hero-img">
+              <figure>
+                <img src={HeroImg} alt="" className='img-style' />
+              </figure>
+            </div>
+         
         </div>
+      </div>
     );
 };
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import FetureProductsComp from '../../components/Home/FeatureProducts/FetureProducts';
 import HelpComp from '../../components/Home/Help/Help';
+import Hero from '../../components/Home/Hero/Hero';
 
 const Home = () => {
     return (
         <div>
-            <FetureProductsComp></FetureProductsComp>
-            <HelpComp></HelpComp>
+            <Hero></Hero>
+           
         </div>
     );
 };
