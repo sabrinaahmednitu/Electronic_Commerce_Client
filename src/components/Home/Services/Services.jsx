a3c7c5585232eb1b011abaef19d878396a0222b6
sabrina <sabrinaahmednitu@gmail.com>
2023-09-17 01:07:49 +0600

    services done

--- src/components/Home/Services/Services.jsx
+++ src/components/Home/Services/Services.jsx
@@ -1,36 +1,31 @@
 import React from 'react';
-import './Services.css'
+import './Services.css';
 
 const Services = () => {
-    return (
-      <div className="container grid lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-1 gap-5 mx-auto">
-        <div className="services-column-1">
-          <div className="flex justify-center items-center flex-col">
-            <i class="fa-solid fa-car"></i>
-            <h1>Super Fast and Free Delivery</h1>
-          </div>
+  return (
+    <div className="max-w-6xl container services-main grid lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-1 mx-auto pt-12 mb-12">
+      <div className="services-1">
+        <i class="fa-solid fa-car text-indigo-600 text-2xl"></i>
+        <h1>Super Fast and Free Delivery</h1>
+      </div>
+
+      <div className="services-2 services-column-2">
+        <div className="row-1">
+          <i class="fa-solid fa-shield-halved text-indigo-600 text-2xl"></i>
+          <h1>Non-contact Shipping</h1>
         </div>
-        <div
-          className="services-column-2 flex justify-center items-center flex-col gap-3
-        "
-        >
-          <div className="row-1 flex justify-center items-center flex-row gap-5">
-            <i class="fa-solid fa-shield-halved"></i>
-            <h1>Non-contact Shipping</h1>
-          </div>
-          <div className="row-2 flex justify-center items-center flex-row gap-5">
-            <i class="fa-solid fa-shield-halved"></i>
-            <h1>Money-back Guranteed</h1>
-          </div>
-        </div>
-        <div className="services-column-1">
-          <div className="flex justify-center items-center flex-col">
-            <i class="fa-solid fa-file-shield"></i>
-            <h1>Super Secure Payment System</h1>
-          </div>
+        <div className="row-1">
+          <i class="fa-solid fa-shield-halved text-indigo-600 text-2xl"></i>
+          <h1>Money-back Guranteed</h1>
         </div>
       </div>
-    );
+
+      <div className="services-3">
+        <i class="fa-solid fa-file-shield text-indigo-600 text-2xl"></i>
+        <h1>Super Secure Payment System</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Services;
