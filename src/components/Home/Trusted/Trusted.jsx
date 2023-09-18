840317314f388b3f463152d59255d31f64621b2b
sabrina <sabrinaahmednitu@gmail.com>
2023-09-18 21:11:22 +0600

    services responsive and trusted comp done

--- src/components/Home/Trusted/Trusted.jsx
+++ src/components/Home/Trusted/Trusted.jsx
@@ -1,10 +1,46 @@
 import React from 'react';
+import './Trusted.css'
 
 const Trusted = () => {
     return (
-        <div>
-          <h1>Trusted</h1>
+      <div className="brand-section">
+        <div className="container m-auto">
+          <h3>Trusted by 100+ Companies</h3>
+          <div className="brand-section-logo">
+            <div className="brand-logo">
+              <img
+                src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image2.png"
+                alt="trusted-brands"
+                alt=""
+              />
+            </div>
+            <div className="brand-logo">
+              <img
+                src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image3.png"
+                alt="trusted-brands"
+              />
+            </div>
+            <div className="brand-logo">
+              <img
+                src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image4.png"
+                alt="trusted-brands"
+              />
+            </div>
+            <div className="brand-logo">
+              <img
+                src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image6.png"
+                alt="trusted-brands"
+              />
+            </div>
+            <div className="brand-logo">
+              <img
+                src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image8.png"
+                alt="trusted-brands"
+              />
+            </div>
+          </div>
         </div>
+      </div>
     );
 };
 
