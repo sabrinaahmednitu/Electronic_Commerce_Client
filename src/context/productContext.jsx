8452d11d2c9b2b99a1c8ab9e772f8689377c488c
sabrina <sabrinaahmednitu@gmail.com>
2023-09-25 22:29:58 +0600

    axios

--- src/context/productContext.jsx
+++ src/context/productContext.jsx
@@ -1,9 +1,25 @@
-import { createContext, useContext } from 'react';
+import axios from 'axios';
+import { createContext, useContext, useEffect } from 'react';
 
 const AppContext = createContext();
 
+const API = "https://api.pujakaitem.com/api/products";
+
 //delivery man
 const AppProvider = ({ children }) => {
+
+    const getProducts = async (url) => {
+        const res = await axios.get(url);;
+        const products = await res.data;
+        console.log(products);
+    };
+
+
+    useEffect(() => {
+        getProducts(API);;
+    }, []);
+    
+
     return (
       <AppContext.Provider value={{ myName: 'Sabrina Ahmed Nitu' }}>
         {children}
