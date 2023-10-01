4945ca23354e714f4aae07f275d81401c86c6299
sabrina <sabrinaahmednitu@gmail.com>
2023-10-02 03:00:01 +0600

    single product data get

--- src/pages/SingleProduct/SingleProduct.jsx
+++ src/pages/SingleProduct/SingleProduct.jsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { useProductContext } from '../../context/productContext';
 
 const SingleProduct = () => {
   const { id } = useParams();
+  const { getSingleProduct, isSingleLoading, singleProduct } = useProductContext();
+console.log(singleProduct);
+  useEffect(() => {
+    getSingleProduct(`https://api.pujakaitem.com/api/products?id=${id}`);
+  }, []);
+
   return (
     <div>
       <h1>SingleProduct { id}</h1>
