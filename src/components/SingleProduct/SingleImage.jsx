419f2d8e5680ebf8695432962c7d0d0134a1742d
sabrina <sabrinaahmednitu@gmail.com>
2023-10-07 00:00:28 +0600

    img fetch done

--- src/components/SingleProduct/SingleImage.jsx
+++ src/components/SingleProduct/SingleImage.jsx
@@ -5,7 +5,18 @@ const SingleImage = ({ imgs }) => {
     return (
       <div>
         <h1>images</h1>
-       <h2>all img will loaded soon</h2>
+        <div className="flex flex-col">
+          {/* {imgs.map((img, index) => (
+            <img key={index} src={img.url} alt={img.filename} />
+          ))} */}
+          {Array.isArray(imgs) && imgs.length > 0 ? (
+            imgs.map((img, index) => (
+              <img key={index} src={img.url} alt={img.filename} />
+            ))
+          ) : (
+            <p>No images available</p>
+          )}
+        </div>
       </div>
     );
 };
