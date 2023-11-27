23c3faf7ac225ca8bdc42138375f2e745fa9f156
sabrina <sabrinaahmednitu@gmail.com>
2023-11-28 00:03:17 +0600

    listview design running

--- src/components/Product/ListView/ListView.jsx
+++ src/components/Product/ListView/ListView.jsx
@@ -1,19 +1,36 @@
 import React from 'react';
 import './ListView.css';
+import FormatPrice from '../../../Helpers/FormatPrice';
+import { NavLink } from 'react-router-dom';
 
 const ListView = ({ products }) => {
-    
-    return (
-        <div>
-            <div className="container grid lg:grid-cols-1">
-                {
-                    products.map((product) => {
-                        const{id,name,price,description,image}
-                    })
-                }
-           </div>
-        </div>
-    );
+  return (
+    <div>
+      <div className="container grid lg:grid-cols-1">
+        {products.map((product) => {
+          const { id, name, price, description, image } = product;
+          return (
+            <div className="card grid grid-cols-2">
+              <figure className="w-50">
+                <img src={image} alt="" />
+              </figure>
+              <div>
+                <h1>{name}</h1>
+                <p>
+                  {' '}
+                  <FormatPrice price={price}></FormatPrice>{' '}
+                </p>
+                <p>{description.slice(0, 99)}...</p>
+                <NavLink to={`/singleproduct/${id}`}>
+                  <button className="btn">Read More</button>
+                </NavLink>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default ListView;
