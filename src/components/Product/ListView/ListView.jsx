4eef286266bbbb9eade1f39e22a33a73aef7dcd1
sabrina <sabrinaahmednitu@gmail.com>
2023-11-27 23:52:34 +0600

    listview

--- src/components/Product/ListView/ListView.jsx
+++ src/components/Product/ListView/ListView.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './ListView.css';
 
-const ListView = () => {
+const ListView = ({ products }) => {
+    
     return (
         <div>
-            
+            <div className="container grid lg:grid-cols-1">
+                {
+                    products.map((product) => {
+                        const{id,name,price,description,image}
+                    })
+                }
+           </div>
         </div>
     );
 };
--- src/components/Product/ProductList/ProductList.jsx
+++ src/components/Product/ProductList/ProductList.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import { useFilterContext } from '../../../context/filterContext';
 import GridView from '../GridView/GridView';
+import ListView from '../ListView/ListView';
 
 const ProductList = () => {
-    const { filter_products ,setGridView } = useFilterContext();
+    const { filter_products, grid_view } = useFilterContext();
     // console.log(filter_products);
   
-  if (setGridView) {
+  if (grid_view === true) {
     return <GridView products={filter_products}></GridView>;
   }
-  // if (setGridView === false) {
-  //   return <ListView products={filter_products}></ListView>;
-  // }
+  if (grid_view === false) {
+    return <ListView products={filter_products}></ListView>;
+  }
    
 };
 
