5e44b1bdd6c35347378031dae8058a98fd26dc44
sabrina <sabrinaahmednitu@gmail.com>
2023-12-09 22:10:26 +0600

    35 done

--- src/components/Product/FilterSection/FilterSection.jsx
+++ src/components/Product/FilterSection/FilterSection.jsx
@@ -3,30 +3,56 @@ import { useFilterContext } from '../../../context/filterContext';
 import './FilterSection.css'
 const FilterSection = () => {
   const {
-    filters: { text }, updateFilterValue, all_products } = useFilterContext();
+    filters: { text , category  }, updateFilterValue, all_products } = useFilterContext();
   
   //to get the unique fields
   const getUniqueData = (data, property) => {
     let newValue = data.map((curElem) => {
       return curElem[property];
     });
-    newValue = ["All" , ...new Set(newValue)] //unique value asbe
-    console.log(newValue);
+    return newValue = ["All" , ...new Set(newValue)] //unique value asbe
+    // console.log(newValue);
   }
   
   //we need unique data
-  const categoryOnlyData = getUniqueData(all_products,"category");
+  const categoryOnlyData = getUniqueData(all_products, "category");
+  
     return (
       <div>
+        {/* --------search part--------- */}
         <div className="filter-search">
           <form onSubmit={(e) => e.preventDefault()}>
-            <input type="text"
+            <input
+              type="text"
               name="text"
               name="text"
               onChange={updateFilterValue}
-            placeholder='Search'/>
+              placeholder="Search"
+            />
           </form>
-       </div>
+        </div>
+
+        {/* --------Category wise filter part--------- */}
+        <div className="filter-category">
+          <h4>Category</h4>
+          <div className='category-name'>
+            {
+              categoryOnlyData.map((curElem ,index) => {
+                return (
+                  <button
+                    key={index}
+                    type="button"
+                    name="category"
+                    value={curElem}
+                    onClick={updateFilterValue}
+                  >
+                    {curElem}
+                  </button>
+                );
+              })
+            }
+          </div>
+        </div>
       </div>
     );
 };
