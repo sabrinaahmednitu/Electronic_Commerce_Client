2c72a8144a5f4f80828ee650ac8447826fb46689
sabrina <sabrinaahmednitu@gmail.com>
2023-12-13 03:51:49 +0600

    making add to cart array

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { useCartContext } from '../../context/cartContext';
 
 const Cart = () => {
+    const { cart } = useCartContext();
+    console.log(cart);
     return (
-        <div>
-            <h2>i am cart</h2>
-        </div>
+      
+            <div className="container mx-auto">
+                <div className="car-heading grid lg:grid-cols-5 text-center">
+                    <p>Item</p>
+                    <p className="cart-hide">Price</p>
+                    <p>Quantity</p>
+                    <div className="cart-hide">Subtotal</div>
+                    <p>Remove</p>
+                </div>
+                <hr />
+            </div>
+     
     );
 };
 
--- src/reducer/cartReducer.jsx
+++ src/reducer/cartReducer.jsx
@@ -3,7 +3,23 @@
 const cartReducer = (state, action) => {
     if (action.type === "ADD_TO_CART") {
         let { id, currentColor, amount, singleProduct } = action.payload;
-        console.log('add te cart', singleProduct);
+        // console.log('add to cart', singleProduct);
+        let cartProduct;
+
+        cartProduct = {
+          id: id + currentColor, //id different hobe color er jonne
+          name: singleProduct.name,
+          color: currentColor,
+          amount: amount,
+          image: singleProduct.image[0].url,
+          price: singleProduct.price,
+          max: singleProduct.stock,
+        };
+
+        return {
+            ...state,
+            cart:[...state.cart , cartProduct],
+        }
     }
     return state;
 };
