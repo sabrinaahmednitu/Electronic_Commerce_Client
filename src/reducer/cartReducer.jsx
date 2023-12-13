10ddac42ea6c5e02940b62dee48f4acfd7962134
sabrina <sabrinaahmednitu@gmail.com>
2023-12-13 15:45:22 +0600

    cart stock wise

--- src/reducer/cartReducer.jsx
+++ src/reducer/cartReducer.jsx
@@ -3,23 +3,50 @@ const cartReducer = (state, action) => {
   if (action.type === 'ADD_TO_CART') {
     let { id, currentColor, amount, singleProduct } = action.payload;
     // console.log('add to cart', singleProduct);
-    let cartProduct;
+  
+    //tackel existing product
+    let existingProduct = state.cart.find((curItem) => curItem.id = id + currentColor);
+    
+    if (existingProduct) {
+      let updatedProduct = state.cart.map((curElem) => {
+        if (curElem.id = id + currentColor) {
+          let newAmount = curElem.amount + amount;
+          // if (newAmount >= curElem.max) { //max=stock
+          //   newAmount=curElem.max
+          // }
+           return {
+             ...curElem,
+             amount: newAmount,
+           };
+        }
+        else {
+            return curElem
+        }
+       
+      })
+       return {
+         ...state,
+         cart: updatedProduct
+       };
+    } else {
+      let cartProduct = {
+        id: id + currentColor, //id different hobe color er jonne
+        name: singleProduct.name,
+        color: currentColor,
+        amount: amount,
+        image: singleProduct.image[0].url,
+        price: singleProduct.price,
+        max: singleProduct.stock,
+      };
 
-    cartProduct = {
-      id: id + currentColor, //id different hobe color er jonne
-      name: singleProduct.name,
-      color: currentColor,
-      amount: amount,
-      image: singleProduct.image[0].url,
-      price: singleProduct.price,
-      max: singleProduct.stock,
-    };
+      return {
+        ...state,
+        cart: [...state.cart, cartProduct],
+      };
+    }
+    }
 
-    return {
-      ...state,
-      cart: [...state.cart, cartProduct],
-    };
-  }
+
 
   // REMOVE ITEM
   if (action.type === 'REMOVE_ITEM') {
